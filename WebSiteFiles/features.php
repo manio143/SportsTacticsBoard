bcf0583a99e6689df5d478bcbe34bd98a55f5621
rdturner0 <rdturner0@040a5328-f121-0410-ac9e-df302b297ff3>
2006-12-21 04:20:37 +0000

    Updated web pages to reflect new functionality and planned features. Now published on web site yet as the newly implemented features are not available in the released packages.

--- WebSiteFiles/features.php
+++ WebSiteFiles/features.php
@@ -14,23 +14,27 @@
         <li>Records sequences of positions for documenting tactical strategies or plays.</li>
         <li>Save and loads tactical sequences from files - allows building a library of plays or drills.</li>
         <li>Supports pre-defined layouts that position players, ball/puck and officials.</li>
+        <li>Copying current layout to clipboard as an image for easily importing into other programs, such as PowerPoint.</li>
       </ul>
       
       <br />
       
       <h2>Planned Features</h2>
-      <ul>
-        <li>Exporting image sequences as bitmaps.</li>
-        <li>Copying current image to clipboard for easily importing into other programs, such as PowerPoint.</li>
-        <li>Pre-defined library of tactical sequences built by user community.</li>
-        <li>Support for other sports (basketball, football, etc.).</li>
+      <ol>
+        <li>Ensure that &quot;Saved Layouts&quot; are only available for the currently selected sports field type.</li> 
+        <li>Implement saving and loading of &quot;Saved Layouts&quot; as they currently don't save to disk.</li>
+        <li>Improved layout sequence creation and management.</li>
         <li>Support for &quot;views&quot; of sections of the playing field, including rotation.</li>
         <li>Improved drawing of fields, players and movement lines.</li>
-        <li>More configurable and flexible movements lines.</li>
+        <li>Exporting image sequences as bitmaps.</li>
+        <li>Support for other sports (basketball, football, etc.).</li>
+        <li>Support for predefined &quot;Field Views&quot; for limited views of areas of the playing field.</li>
         <li>Movement line types, allowing for passing, carrying, dribbling, heading, etc to be distinguished more easily.</li>
+        <li>More configurable and flexible movements lines.</li>
         <li>Support for marker lines associated with field objects (easily show offside line in soccer, etc.).</li>
         <li>Support for annotated descriptions and labels placed on the field of play.</li>
-      </ul>
+        <li>Pre-defined library of tactical sequences built by user community.</li>
+      </ol>
 
       <h2>Release Plan</h2>
       <p>TBD<p>
