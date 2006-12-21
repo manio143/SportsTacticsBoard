d0a0680b8896fcedb03b307b93903c2066e2a7e3
rdturner0 <rdturner0@users.noreply.github.com>
2006-12-21 05:53:18 +0000

    Updated version to 0.1.90 in slow preparation for 0.2 release.

--- SportsTacticsBoard/Properties/AssemblyInfo.cs
+++ SportsTacticsBoard/Properties/AssemblyInfo.cs
@@ -55,8 +55,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.1.*")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.1.90.*")]
+[assembly: AssemblyFileVersion("0.1.90.0")]
 
 // Mark the assembly as being CLS compliant
 [assembly: CLSCompliant(true)]
