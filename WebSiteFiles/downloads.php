bf985d68d6fc7056c0a3fc90f2ac714fef0fa8ae
rdturner0 <rdturner0@040a5328-f121-0410-ac9e-df302b297ff3>
2010-01-21 04:27:04 +0000

    Updated web site files

--- WebSiteFiles/downloads.php
+++ WebSiteFiles/downloads.php
@@ -11,17 +11,17 @@
 
       <p>Downloads are available from the <a href="http://sourceforge.net/project/showfiles.php?group_id=181975">SourceForge download page</a>. The following links are provided for easier access to the most recent releases.</p>
 
-      <h3>Windows Installation Package</h3>
+      <h3>Windows Installation Packages</h3>
       <p>This will be the preferred method of installation for most people as it's the easiest to install and uninstall.</p>
-      <p>The most current release: <a href="http://downloads.sourceforge.net/sportstacticsbd/SportsTacticsBoard-0.2.1005-setup.exe">SportsTacticsBoard-0.2.1005-setup.exe</a></p>
-      <p>The most current release: <a href="http://downloads.sourceforge.net/sportstacticsbd/SportsTacticsBoard-0.2.1005.msi">SportsTacticsBoard-0.2.1005.msi</a></p>
+      <p>Self-extracting installer (easiest): <a href="http://downloads.sourceforge.net/sportstacticsbd/SportsTacticsBoard-0.2.1005-setup.exe">SportsTacticsBoard-0.2.1005-setup.exe</a></p>
+      <p>MSI install file: <a href="http://downloads.sourceforge.net/sportstacticsbd/SportsTacticsBoard-0.2.1005.msi">SportsTacticsBoard-0.2.1005.msi</a></p>
 
       <h3>Binaries</h3>
-      <p>The most current release is: <a href="http://downloads.sourceforge.net/sportstacticsbd/SportsTacticsBoard-0.2.1005-bin.zip">SportsTacticsBoard-0.2.1005-bin.zip</a></p>
+      <p><a href="http://downloads.sourceforge.net/sportstacticsbd/SportsTacticsBoard-0.2.1005-bin.zip">SportsTacticsBoard-0.2.1005-bin.zip</a></p>
       <p>See the readme.txt file for installation instructions.</p>
       
       <h3>Source Code</h3>
-      <p>The most current release is: <a href="http://downloads.sourceforge.net/sportstacticsbd/SportsTacticsBoard-0.2.1005-src.zip">SportsTacticsBoard-0.2.1005-src.zip</a></p>
+      <p><a href="http://downloads.sourceforge.net/sportstacticsbd/SportsTacticsBoard-0.2.1005-src.zip">SportsTacticsBoard-0.2.1005-src.zip</a></p>
       <p>See the readme.txt file for installation instructions.</p>
       <p>Source code can be compiled on Windows without any need for a development environment. You only need the .NET 3.5 Framework runtime installed. A batch file is included in the source tree (see notes in ReadMe.txt file) to build the program.</p>
       <p>Source code can also be downloaded using normal SourceForge.NET repository 
--- WebSiteFiles/features.php
+++ WebSiteFiles/features.php
@@ -7,43 +7,30 @@
 <body>
 <?php include("header.php"); ?>
 
-
-      <h2>Features (Release 0.2.1005)</h2>
+      <h2>Features (0.2.1005)</h2>
       <ul>
         <li>Supports more than one sport (currently Soccer, Hockey, Volleyball), more to be added.</li>
         <li>Records sequences of positions for documenting tactical strategies or plays.</li>
         <li>Save and loads tactical sequences from files - allows building a library of plays or drills.</li>
-	<li>Supports pre-defined layouts that position players, ball/puck and officials.</li>
-	<li>Supports exporting images and image sequences to files or clipboard.</li>
+	      <li>Supports pre-defined layouts that position players, ball/puck and officials.</li>
+	      <li>Supports exporting images and image sequences to files or clipboard.</li>
       </ul>
       <p>See the <a href="changelog.txt">change log</a> for more details. This documents all changes committed to source control, which may include additional functionality after the release above.</p>
       
       <br />
       
-      <h2>Release Plan</h2>
-
-      <p>The following are descriptions of the planned content of upcoming releases. Projected dates for these
-      releases are not possible at this time due to limited programming resources. They will be released when the 
-      features are complete.</p>
-
-      <h3>Release 0.3</h3>
+      <h2>Potential Future Features</h2>
       <ul>
         <li>Improve drawing of fields, players and movement lines.</li>
         <li>Support for &quot;views&quot; of sections of the playing field, including rotation.</li>
         <li>Support for predefined &quot;Field Views&quot; for limited views of areas of the playing field.</li>
-        <li>Support NFL football or NBA basketball.</li>
         <li>Movement line types, allowing for passing, carrying, dribbling, heading, etc to be distinguished more easily.</li>
-      </ul>
-
-      <h2>Other Planned Features Not Yet Scheduled</h2>
-      <ul>
-        <li>Support for additional sports.</li>
+        <li>Support for additional sports fields (American football, basketball, rugby, etc.).</li>
         <li>More configurable and flexible movements lines.</li>
         <li>Support for marker lines associated with field objects (easily show offside line in soccer, etc.).</li>
         <li>Support for annotated descriptions and labels placed on the field of play.</li>
-        <li>Pre-defined library of tactical sequences built by user community.</li>
+        <li>Pre-defined libraries of tactical sequences built by user community.</li>
       </ul>
-
 
 
 <?php include("footer.php"); ?>
